55c703abb1afae2792661081aadcbc794a2fee51
RapidsT3 <middleforkrapids@yahoo.co.jp>
2011-04-02 04:28:24 +0900

    [Verde] update:

--- Utility/ElementsContainer.cs
+++ Utility/ElementsContainer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using System.Text.RegularExpressions;
 
 namespace Verde.Utility
 {
@@ -20,6 +21,7 @@ namespace Verde.Utility
         public int Good { set; get; }
         public int Bad { set; get; }
         public int Comment { set; get; }
+        public int Pickup { set; get; }
 
         public Element()
         {
@@ -27,33 +29,70 @@ namespace Verde.Utility
 
         public void Import(XElement xmlElement)
         {
-            var link = xmlElement.Element(HtmlParser.nsXhtml + "a");
-            var image = link.Element(HtmlParser.nsXhtml + "img");
-
             int nCount = 0;
             foreach (var item in xmlElement.Descendants()) {
                 if (item.Name.Equals(HtmlParser.nsXhtml + "a")) {
                     this.UrlEntry = item.Attribute("href").Value;
-                } else if (item.Name.Equals(HtmlParser.nsXhtml + "img")) {
-                    if (item.Attribute("class").Value == "thumb") {
-                        string strUrl = item.Attribute("src").Value;
-                        if (String.Compare("http:", 0, strUrl, 0, 5) != 0) {
-                            if (Element.bLargeThumb == true) {
-                                /* 現状決め打ち "/i"を除外する */
-                                strUrl = strUrl.Substring(2);
-                            }
-                            strUrl = "http://pya.cc" + strUrl;
+                } else if (item.Name.Equals(HtmlParser.nsXhtml + "img") && item.Attribute("class").Value == "thumb") {
+                    string strUrl = item.Attribute("src").Value;
+                    if (String.Compare("http:", 0, strUrl, 0, 5) != 0) {
+                        if (Element.bLargeThumb == true) {
+                            /* 現状決め打ち "/i"を除外する */
+                            strUrl = strUrl.Substring(2);
                         }
-                        this.UrlThumbnail = strUrl;
-                        //this.dbImageCache.GetCache(strUrl, this.CheckPath);
-                        //this.dbImageCache.GetImageCache(strUrl, this.DrawThumbImage);
+                        strUrl = "http://pya.cc" + strUrl;
                     }
-                } else if (item.Name.Equals(HtmlParser.nsXhtml + "span")) {
+                    this.UrlThumbnail = strUrl;
+                    //this.dbImageCache.GetCache(strUrl, this.CheckPath);
+                    //this.dbImageCache.GetImageCache(strUrl, this.DrawThumbImage);
+                } else if (item.Name.Equals(HtmlParser.nsXhtml + "span") && item.Attribute("class").Value == "block") {
+                    List<int> listNumbers = new List<int>();
                     switch (nCount++) {
-                        case 0: this.Type = item.Value; break;
-                        case 1: this.Title = item.Value; break;
-                        case 2: this.Poster = item.Value; break;
-                        //case 3: this.Type = item.Value; break;
+                        case 0: this.Type = this.GetLastWord(item.Value); break;
+                        case 1: this.Title = this.GetInnerWord(item.Value, '「', '」'); break;
+                        case 2: this.Poster = this.GetDelimitedWord(item.Value, ':'); break;
+                        case 3: this.GetNumbers(item.Value, ':', listNumbers); break;
+                    }
+                }
+            }
+        }
+
+        private string GetInnerWord(string strWord, char cBegin, char cEnd)
+        {
+            int nBegin = strWord.IndexOf(cBegin) + 1;
+            int nEnd = strWord.IndexOf(cEnd);
+            return strWord.Substring(nBegin, nEnd - nBegin);
+        }
+
+        private string GetLastWord(string strWords)
+        {
+            for (var i = 1; i < strWords.Length; i++) {
+                string strRet = strWords.Substring(strWords.Length - i);
+                if (Regex.IsMatch(strRet, @"^[a-zA-Z0-9]+$") == false) {
+                    return strRet.Substring(1);
+                }
+            }
+            return string.Empty;
+        }
+
+        private string GetDelimitedWord(string strWords, char cDelimiter)
+        {
+            return strWords.Substring(strWords.IndexOf(cDelimiter) + 1);
+        }
+
+        private void GetNumbers(string strWords, char cDelimiter, List<int> listNumbers)
+        {
+            string strTmp = strWords;
+            for (;;) {
+                int nPos = strTmp.IndexOf(cDelimiter);
+                if (nPos == 0) break;
+                string strNumber = strTmp.Substring(nPos + 1);
+                for (var i = 0; i < strNumber.Length; i++) {
+                    strNumber = strNumber.Substring(0, strWords.Length - i);
+                    if (Regex.IsMatch(strNumber, @"^[0-9]+$") == true) {
+                        listNumbers.Add(int.Parse(strNumber));
+                        strTmp = strTmp.Substring(strWords.Length - i);
+                        break;
                     }
                 }
             }
